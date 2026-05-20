c383b0e159fd32bdfdeac423627cc5d7363d389e
turboyjtx <turboyjtx@gmail.com>
2026-05-20 12:22:07 +0800

    2.11.71-qianguo

--- TurboNetSDK.framework/Headers/TurboShortVideo.h
+++ TurboNetSDK.framework/Headers/TurboShortVideo.h
@@ -16,11 +16,26 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (instancetype)sharedInstance;
 
-//初始化短视频sdk
--(void)initWithCompletionHandler:(ShortVideoCompletionHandler _Nullable)completionHandler;
+//初始化短视频sdk. ⚠️注意:请在广告sdk初始化成功后调用短视频sdk初始化方法
+-(void)startWithCompletionHandler:(ShortVideoCompletionHandler _Nullable)completionHandler;
 
 //获取展示短视频的ViewController
 -(nullable UIViewController *)shortVideoViewController;
+
+
+//为了视频的播放体验，必须在AppDelegate的applicationDidBecomeActive：和applicationWillResignActive：中调用startOpenGLES和stopOpenGLES方法
+//这两个方法不耗时,不做这一步,可能带来未知的黑屏或者crash。示例如下
+/*
+- (void)applicationWillResignActive:(UIApplication *)application {
+    [[TurboShortVideo sharedInstance] stopOpenGLES];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+ [[TurboShortVideo sharedInstance] startOpenGLES];
+}
+ */
+-(void)startOpenGLES;
+-(void)stopOpenGLES;
 
 - (instancetype)init NS_UNAVAILABLE;
 @end
